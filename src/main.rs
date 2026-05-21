6a0f8972cf8046edd3f9d643100dbd23c8ffec7d
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-05-22 02:13:10 +0700

    refuse to run as root

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ async fn greet() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    if uzers::get_effective_uid() == 0 {
+        eprintln!("refusing to run as euid 0 (root)");
+        std::process::exit(1);
+    } else if uzers::get_effective_gid() == 0 {
+        eprintln!("refusing to run with egid 0 (root group)");
+        std::process::exit(1);
+    }
+
     let Some(uds_path) = tor_site::env::get_uds_path() else {
         eprintln!(
             "environment variable {} must be set to where you want to listen to",
